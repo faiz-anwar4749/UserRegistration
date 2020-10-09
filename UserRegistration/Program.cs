e53cdbba65e7fc68bf8a3e4c2a119451d4f41a0c
Faiz <faiz.anwar4749@gmail.com>
2020-10-09 13:08:43 +0530

    [Faiz] Refactor: Validating User Last name using REGEX

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -11,6 +11,9 @@ namespace UserRegistration
             Console.Write("Enter User First Name: ");
             string firstName = Console.ReadLine();
             FirstNameCheck(firstName);
+            Console.Write("Enter User Last Name: ");
+            string lastName = Console.ReadLine();
+            LastNameCheck(lastName);
         }
         public static void FirstNameCheck(string firstName)
         {
@@ -24,6 +27,20 @@ namespace UserRegistration
             else
             {
                 Console.WriteLine("User First Name is Invalid.");
+            }
+        }
+        public static void LastNameCheck(string lastName)
+        {
+            string pattern = "^[A-Z]+[a-z A-Z]{2,}$";
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(lastName);
+            if (match.Success)
+            {
+                Console.WriteLine("User Last Name entered successfully.");
+            }
+            else
+            {
+                Console.WriteLine("User Last Name is Invalid.");
             }
         }
     }
