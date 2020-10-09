3065654836d16bbe6c1acfbc39506c1aebe0c19c
Faiz <faiz.anwar4749@gmail.com>
2020-10-09 13:01:31 +0530

    [Faiz] Refactor : Validating User First Name using REGEX

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace UserRegistration
 {
@@ -6,7 +7,24 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to User Registration!");
+            Console.WriteLine("Welcome to User Registraton");
+            Console.Write("Enter User First Name: ");
+            string firstName = Console.ReadLine();
+            FirstNameCheck(firstName);
+        }
+        public static void FirstNameCheck(string firstName)
+        {
+            string pattern = "^[A-Z]+[a-z A-Z]{2,}$";
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(firstName);
+            if (match.Success)
+            {
+                Console.WriteLine("User First Name entered successfully.");
+            }
+            else
+            {
+                Console.WriteLine("User First Name is Invalid.");
+            }
         }
     }
 }
